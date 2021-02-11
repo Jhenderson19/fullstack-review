38731b2fefa18e454d01a818a6c0cab09fd8068a
James Henderson <jhende1995@gmail.com>
2021-02-11 14:27:53 -0500

    fixed bug in search

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -9,9 +9,7 @@ class Search extends React.Component {
   }
 
   onChange (e) {
-    this.setState({
-      term: e.target.value
-    });
+    this.state.term = e.target.value;
   }
 
   search() {
@@ -21,9 +19,9 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>
       <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    </div>)
   }
 }
 
